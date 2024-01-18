7df817c0b121d20daa188386eef54acb57db9fa7
Ryder Dettloff <dettloff.ryder@student.greenriver.edu>
2024-01-18 02:24:49 -0500

    updated coonfig and added index page

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-echo "My Diner";
+/* Ryder Dettloff
+* January 2024
+* https://ryderdettloff.greenriverdev.com/328/diner/
+* This is my Controller
+*/
+
+//Display Errors
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+
+require_once ('vendor/autoload.php');
+//instance method
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    //echo "My Diner";
+    $view= new Template();
+    echo $view->render('views.home.html');
+});
+//run fat-free
+$f3->run();
+
+
+?>
